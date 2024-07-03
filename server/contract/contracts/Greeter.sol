da297d889b41e10a8df062e03976eda9cf349adb
20120077 <ngqghien@gmail.com>
2024-07-03 19:23:32 +0700

    add greeter sol

--- server/contract/contracts/Greeter.sol
+++ server/contract/contracts/Greeter.sol
@@ -6,20 +6,34 @@ import "hardhat/console.sol";
 contract Greeter {
     string private greeting;
 
+    /**
+     * @dev Constructor function that initializes the Greeter contract with a greeting message.
+     * @param _greeting The initial greeting message.
+     */
     constructor(string memory _greeting) {
         console.log("Deploying a Greeter with greeting:", _greeting);
         greeting = _greeting;
     }
 
+    /**
+     * @dev Retrieves the current greeting message.
+     * @return The current greeting message.
+     */
     function greet() public view returns (string memory) {
         return greeting;
     }
 
+    /**
+     * @dev Updates the greeting message.
+     * @param _greeting The new greeting message.
+     */
     function setGreeting(string memory _greeting) public {
         console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
         greeting = _greeting;
     }
 
+    /**
+     * @dev Allows the contract to receive Ether.
+     */
     function deposit() public payable {}
-
 }
